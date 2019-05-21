3dc79af857fa97d8008a1fcdc151ad940bbbad4d
nirajkale157@outlook.com <nirajkale157@outlook.com>
2019-05-22 00:50:56 +0530

    Added test cases

--- test_controller.py
+++ test_controller.py
@@ -1,5 +1,42 @@
 from pynet import *
 
-c = Controller()
-client1 = c.get_client()
-client1.ping('hello')
+if __name__ == '__main__':
+
+    try:
+        gateway_ip = '192.168.0.105'
+        controller = Controller(gateway_ip = gateway_ip)
+        client1 = controller.get_client()
+        bk.safe_print('pinging handler')
+        client1.ping()
+        d = r'C:\Users\703235761\Documents\License'
+
+        bk.safe_print('\n\nDownloading files form gateway..')
+        client1.get_files_from_gateway(folder = d)
+
+        bk.safe_print('\n\nUploading files to gateway..')
+        client1.send_files_to_gateway(dirname='test', files = [os.path.join(d,f) for f in os.listdir('Pictures')])
+        
+        bk.safe_print('\n\nTest for receiving subroutine streaming..')
+        client1.get_subroutine_stream(name='str_sensor',\
+                              callback= sample_callback,\
+                              eos_callback= sample_eos_callback,\
+                              arguments=[4, 55]
+                        )
+
+        bk.safe_print('\n\nTest for receiving subroutine data batch ..')
+        bk.safe_print(bk.bytes_to_str( client1.get_subroutine_batch(name='str_sensor_batch',arguments=['123'])))
+        bk.safe_print(bk.bytes_to_str( client1.get_subroutine_batch(name='str_sensor_batch',arguments=['456'])))
+
+        bk.safe_print('\n\nTest for sending subroutine data batch ..')
+        buffer = bk.str_to_bytes('sample data')
+        bk.safe_print(client1.send_subroutine_batch(name='set_io', buffer = buffer ,arguments=[2,True]))
+        bk.safe_print(client1.send_subroutine_batch(name='test'))
+
+        input('Testing completed, Press enter to close gateway')
+        bk.safe_print('closing gateway..')
+        client1.close_gateway()
+        
+    except Exception as e:
+        bk.safe_print('Error in client main')
+        bk.safe_print(e)
+    input('Enter to close')
--- test_gateway.py
+++ test_gateway.py
@@ -1,4 +1,11 @@
 from pynet import *
 
-gw = Gateway(1857)
-gw.start(blocking = True)
+if __name__ == '__main__':
+
+    gateway = Gateway(1857, max_poolsize = 5)
+    gateway.add_subroutine('str_sensor', fetch_sensor_data)
+    gateway.add_subroutine('str_sensor_batch', fetch_sensor_batch_data)
+    gateway.add_subroutine('set_io',set_io)
+    gateway.add_subroutine('test',test)
+    gateway.start(blocking = True)
+    input('Press Enter to close..')
